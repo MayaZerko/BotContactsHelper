0862b1f6fb3eafc489a4f2d4b4d15e030cbb5163
Maya <maya.zerko@gmail.com>
2022-10-04 18:02:08 +0300

    create an entry point function

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 
 
+def main():
+    while True:
+        """
+        Просимо користувача ввести команду для нашого бота
+        Також тут же вимикаємо бота якщо було введено відповідну команду
+        """
+
+        user_input = input('Enter command for bot: ')
+        if user_input == 'Good bye':
+            break
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
